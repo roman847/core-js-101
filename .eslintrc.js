a9bc7e6f6fc176ecdcdd4b420470115626595054
Roman <romabogdan147@gmail.com>
2022-07-22 08:46:35 +0300

    fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,13 +8,13 @@ module.exports = {
   // extends: [
   //   'airbnb-base',
   // ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parserOptions: {
-    ecmaVersion: 2018,
-  },
-  rules: {
-  },
+  // globals: {
+  //   Atomics: 'readonly',
+  //   SharedArrayBuffer: 'readonly',
+  // },
+  // parserOptions: {
+  //   ecmaVersion: 2018,
+  // },
+  // rules: {
+  // },
 };
